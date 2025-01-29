e936b2e2999b0cad88c54fdc1df93917ec25f3b3
Laisfaustt <lais.s.faustino@gmail.com>
2025-01-29 10:39:10 -0300

    Initial commit

---
+++ operadores/aritmeticos.js
@@ -0,0 +1,30 @@
+console.log("ola mundo")
+
+let nome = "juliana"
+var nomes = "juliana"
+console.log(nome)
+
+let idade = 24
+console.log("minha idade é ", idade, "anos")
+
+
+
+let a = 3
+let b = 2
+
+let soma = a + b
+let sub = a - b
+let mult = a * b
+let div = a / b
+let resto = a % b
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
+
+
+
+
+
+
